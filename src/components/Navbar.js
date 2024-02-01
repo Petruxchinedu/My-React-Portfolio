8318680c849781d87bb065633687af88d1ae6845
Petruxchinedu <110899539+Petruxchinedu@users.noreply.github.com>
2024-02-01 09:31:52 +0100

    Updated Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -62,7 +62,7 @@ const Navbar = () => {
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-blue-600'>
-            <a className='flex justify-between items-center w-full text-gray-300' href='/'>
+            <a className='flex justify-between items-center w-full text-gray-300' href='https://peter-resume.tiiny.site/'>
               Resume <BsFillPersonLinesFill size={30}/>
             </a>
           </li>
